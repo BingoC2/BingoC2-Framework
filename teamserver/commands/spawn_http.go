65f6bc03a31ec862c818fb0b4b89b996c1224277
Deranged0tter <becsmith051815+2@gmail.com>
2024-03-12 14:47:24 -0400

    cleanup

--- teamserver/commands/spawn_http.go
+++ teamserver/commands/spawn_http.go
@@ -79,8 +79,6 @@ func SpawnHTTP(c *grumble.Context) error {
 		return err
 	}
 
-	fmt.Println(key)
-
 	// write data to file
 	newBeaconData := yamlstructs.BeaconDataYaml{
 		Name:      name,
@@ -104,41 +102,22 @@ func SpawnHTTP(c *grumble.Context) error {
 		name += ".exe"
 	}
 
-	fmt.Println("test")
-
 	keyA := key[:8]
-	fmt.Println(keyA)
 	keyB := key[8:16]
-	fmt.Println(keyB)
 	keyC := key[16:24]
-	fmt.Println(keyC)
 	keyD := key[24:32]
-	fmt.Println(keyD)
-
-	fmt.Println("test")
 
 	nKeyA := binary.BigEndian.Uint64(keyA)
-	fmt.Println(nKeyA)
 	nKeyB := binary.BigEndian.Uint64(keyB)
-	fmt.Println(nKeyB)
 	nKeyC := binary.BigEndian.Uint64(keyC)
-	fmt.Println(nKeyC)
 	nKeyD := binary.BigEndian.Uint64(keyD)
-	fmt.Println(nKeyD)
-
-	fmt.Println("test3")
 
 	sKeyA := strconv.FormatUint(nKeyA, 10)
 	sKeyB := strconv.FormatUint(nKeyB, 10)
 	sKeyC := strconv.FormatUint(nKeyC, 10)
 	sKeyD := strconv.FormatUint(nKeyD, 10)
 
-	fmt.Println("test4")
-
-	output, err := hellsgopher.CmdReturn(fmt.Sprintf("make -C ./agent/http/%s/ BEACON_NAME=%s KEYA=\"%s\" KEYB=\"%s\" KEYC=\"%s\" KEYD=\"%s\" sSLEEP=%d sJITTER=%d RHOST=%s RPORT=%s LISTENER_NAME=%s USERAGENT=%s URI=%s BINARY_NAME=%s cGOOS=%s cGOARCH=%s SPAWN_PATH=%s", opsys, name, sKeyA, sKeyB, sKeyC, sKeyD, sleep, jitter, RHOST, RPORT, listener, useragent, URI, name, opsys, arch, path))
-
-	fmt.Println("Output: ", string(output))
-	fmt.Println("Error: ", err)
+	hellsgopher.CmdReturn(fmt.Sprintf("make -C ./agent/http/%s/ BEACON_NAME=%s KEYA=\"%s\" KEYB=\"%s\" KEYC=\"%s\" KEYD=\"%s\" sSLEEP=%d sJITTER=%d RHOST=%s RPORT=%s LISTENER_NAME=%s USERAGENT=%s URI=%s BINARY_NAME=%s cGOOS=%s cGOARCH=%s SPAWN_PATH=%s", opsys, name, sKeyA, sKeyB, sKeyC, sKeyD, sleep, jitter, RHOST, RPORT, listener, useragent, URI, name, opsys, arch, path))
 
 	logging.Okay(fmt.Sprintf("Successfully generated beacon (%s)", name), c)
 
