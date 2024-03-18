c14110e5fcd303388fe74a8d3dbb5271577df1b2
Deranged0tter <becsmith051815+2@gmail.com>
2024-03-17 21:58:56 -0400

    debugging

--- teamserver/management/print_time.go
+++ teamserver/management/print_time.go
@@ -7,18 +7,23 @@ import (
 
 func PrintTimePretty(t time.Duration) string {
 	seconds := t.Seconds()
+	fmt.Println(seconds)
 
 	// get the number of hours
 	hours := int(seconds / 60 / 60)
+	fmt.Println(hours)
 
 	// subtract the number of hours from seconds
 	seconds = seconds - float64(hours)
+	fmt.Println(seconds)
 
 	// get minutes
 	minutes := int(seconds / 60)
+	fmt.Println(minutes)
 
 	// subtract the number of minutes from seconds
 	seconds = seconds - float64(minutes)
+	fmt.Println(seconds)
 
 	return fmt.Sprintf("%dh:%dm:%ds", hours, minutes, int(seconds))
 }
